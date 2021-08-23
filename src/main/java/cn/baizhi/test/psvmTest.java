f23620c4b69fb5ec6cb6c9486d8acc1310cf8c4b
jxw <3190717471@qq.com>
2021-08-23 20:52:50 +0800

    ========

--- src/main/java/cn/baizhi/test/psvmTest.java
+++ src/main/java/cn/baizhi/test/psvmTest.java
@@ -2,6 +2,7 @@ package cn.baizhi.test;
 
 public class psvmTest {
     public static void main(String[] args) {
+        System.out.println("=======================");
         System.out.println("你好,世界!");
     }
 }
